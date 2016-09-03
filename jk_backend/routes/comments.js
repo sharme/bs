9fd670b03d385da59fe3310e26577793d0cd5dc1
miwangyao <miwangyao@silkcloud.com>
2016-09-04 01:10:31 +0800

    constant mobile and PC commenting layout

--- jk_backend/routes/comments.js
+++ jk_backend/routes/comments.js
@@ -30,7 +30,7 @@ router.post('/add', function(req, res, next) {
 
 
 router.get('/getCommentsByFSID', function (req, res, next) {
-   var criteriaSQL = mysql.format("select (select u_avatar from jk_users as jku where jku.u_id = jkc.u_id) as u_avatar, (select u_name from jk_users as jku where jku.u_id = jkc.u_id) as u_name, cm_content from jk_comments as jkc where jkc.fs_id = ?", [req.param('fs_id')]);
+   var criteriaSQL = mysql.format("select (select u_avatar from jk_users as jku where jku.u_id = jkc.u_id) as u_avatar, (select u_name from jk_users as jku where jku.u_id = jkc.u_id) as u_name, cm_content, cm_create_time from jk_comments as jkc where jkc.fs_id = ?", [req.param('fs_id')]);
 
     connection.query(criteriaSQL, function (err, result) {
         if(err) {
